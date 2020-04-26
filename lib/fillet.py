6845d12b2c9f67197c62f53a468faf6563b346cf
Be Glass <be.github@com.glass.li>
2020-04-26 12:37:00 +0200

    copyright license added

--- lib/fillet.py
+++ lib/fillet.py
@@ -1,3 +1,33 @@
+#  This file is part of Blender_Shaper_Origin.
+#
+#  Blender_Shaper_Origin is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Blender_Shaper_Origin is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Blender_Shaper_Origin.  If not, see <https://www.gnu.org/licenses/>.
+
+#  This file is part of Blender_Shaper_Origin.
+#
+#  Blender_Shaper_Origin is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Blender_Shaper_Origin is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Blender_Shaper_Origin.  If not, see <https://www.gnu.org/licenses/>.
+
 import math
 from math import pi
 import mathutils
