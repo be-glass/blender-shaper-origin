4c90dc9bd1670105f740372fcfd2d2c77fe692e4
Be Glass <be.github@com.glass.li>
2020-04-29 17:00:52 +0200

    cutout depth

--- lib/shape/mesh_shape.py
+++ lib/shape/mesh_shape.py
@@ -1,10 +1,25 @@
+import math
+
 from . import Shape
+from ..collection import Collection
+from ..helper.other import length
 
 
 class MeshShape(Shape):
 
     def setup(self):
         self.obj.display_type = 'WIRE'
+
+    def update(self):
+        if self.obj.soc_mesh_cut_type == 'Cutout':
+
+            perimeters = Collection.by_obj(self.obj).perimeter_objs()
+
+            if perimeters:
+                cutout_depth = perimeters[0].soc_cut_depth
+
+                if not math.isclose(self.obj.soc_cut_depth, cutout_depth, abs_tol=length('0.01mm')):
+                    self.obj.soc_cut_depth = cutout_depth
 
 # private
 
