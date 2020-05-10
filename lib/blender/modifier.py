93bfac0110073ffc1fe318a9a2cd4dfe14742621
Be Glass <be.github@com.glass.li>
2020-05-10 17:34:22 +0200

    license text

--- lib/blender/modifier.py
+++ lib/blender/modifier.py
@@ -1,3 +1,19 @@
+#   This file is part of Blender_Shaper_Origin.
+#  #
+#   Blender_Shaper_Origin is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#  #
+#   Blender_Shaper_Origin is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#  #
+#   You should have received a copy of the GNU General Public License
+#   along with Blender_Shaper_Origin.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 class Modifier:
     def __init__(self, obj) -> None:
         self.mod = obj.modifiers
