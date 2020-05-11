b8332261b18d5a5873a2330b1268c23541fbc92e
Be Glass <be.github@com.glass.li>
2020-05-11 17:24:22 +0200

    version

--- __init__.py
+++ __init__.py
@@ -21,7 +21,7 @@ bl_info = {
     "name": "Shaper Origin Toolbox",
     "author": "Be Glass",
     "blender": (2, 80, 0),
-    "version": (0, 2, 0),
+    "version": (0, 2, 1),
     "location": "3D View > Sidebar",
     "description": "SVG Export Utilities for cutting lines",
     # "wiki_url": "https://docs.blender.org/manual/en/dev/addons/mesh/3d_print_toolbox.html",
